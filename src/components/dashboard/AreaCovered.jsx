f67e0f2a677d2ea959537bb1af5fbda5c1e0c1b1
Naveen Kumar <73029naveenkumar73537@gmail.com>
2023-11-01 15:54:57 +0530

    adding progressive

--- src/components/dashboard/AreaCovered.jsx
+++ src/components/dashboard/AreaCovered.jsx
@@ -3,17 +3,26 @@ import React from "react";
 const AreaCovered = () => {
   return (
     <div className="container-fluid">
-      <div className="row ">
+      <div className="row d-flex flex-column">
         <div className="col">
-          <div className="d-flex flex-column p-4">
+          <div className="w-100 d-flex flex-column  pt-2">
             <p className="lh-1 p-3">
               <span className="fw-bold">Area Covered (in ha.) </span>
               <br />
               <span className="fw-bold fs-6"> Sep. 2023 - Nov. 2023</span>
             </p>
-            <div className="range-container p-2">
-              <input type="range" className="range-input" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="row align-items-center flex-row">
+            <div className="col-1 fw-bold">
+              Agroforestry
             </div>
+            <div className="col-7 d-flex justify-content-between progress1">
+             <span className="m-0 progress1"></span>
+              <span>10000</span>
+            </div>
+            
           </div>
         </div>
       </div>
--- src/components/dashboard/StatusOperation.jsx
+++ src/components/dashboard/StatusOperation.jsx
@@ -4,53 +4,56 @@ const StatusOperation = () => {
     return (
         <div className="container-fluid ">
             <div className="row">
-                <h3 className="col mt-4 mx-2">Projected Growth</h3>
-            </div>
-            <div className="row">
-                <div className='col mt-5 d-flex align-items-center justify-content-center table-div'>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>State</th>
-                                <th >Total Land Under Management</th>
-                                <th style={{backgroundColor:'rgba(0 ,128,0 ,1)',color:'white'}} >Farming</th>
-                                <th style={{backgroundColor:'rgba(115 ,74,0 ,1)',color:'white'}}>Agroforestry</th>
-                                <th style={{backgroundColor:'rgba(0 ,40,63 ,1)',color:'white'}}>Forestry</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Kerela</td>
-                                <td>5000ha.</td>
-                                <td></td>
-                                <td>5000ha.</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Uttar Pradesh</td>
-                                <td>10000ha.</td>
-                                <td>10000ha.</td>
-                                <td></td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Tamil Nadu</td>
-                                <td>15000ha.</td>
-                                <td></td>
-                                <td>15000ha.</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <td>Rajisthan</td>
-                                <td>6500ha.</td>
-                                <td>5000ha.</td>
-                                <td></td>
-                                <td>1500ha.</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div className='col'>
+                    <h3 className="col mt-4 mx-2">Projected Growth</h3>
+                </div>
+                <div className='col mt-5 d-flex align-items-center justify-content-center'>
+                        <div className='table-responsive'>
+                            <table className='table-bordered text-center '>
+                            <thead>
+                                <tr>
+                                    <th >State</th>
+                                    <th >Total Land Under Management</th>
+                                    <th  style={{ backgroundColor: 'rgba(0, 128, 0, 1)', color: 'white' }} scope="col">Farming</th>
+                                    <th  style={{ backgroundColor: 'rgba(115, 74, 0, 1)', color: 'white' }} scope="col">Agroforestry</th>
+                                    <th  style={{ backgroundColor: 'rgba(0, 40, 63, 1)', color: 'white' }} scope="col">Forestry</th>
+                                </tr>
+                            </thead>
+                            <tbody >
+                                <tr>
+                                    <td >Kerela</td>
+                                    <td >5000ha.</td>
+                                    <td ></td>
+                                    <td >5000ha.</td>
+                                    <td ></td>
+                                </tr>
+                                <tr>
+                                    <td className='w-25'>Uttar Pradesh</td>
+                                    <td>10000ha.</td>
+                                    <td>10000ha.</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>Tamil Nadu</td>
+                                    <td>15000ha.</td>
+                                    <td></td>
+                                    <td>15000ha.</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td>Rajasthan</td>
+                                    <td>6500ha.</td>
+                                    <td>5000ha.</td>
+                                    <td></td>
+                                    <td>1500ha.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
                 </div>
             </div>
+
         </div>
     )
 }
