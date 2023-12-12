325b593a816a79c75d7a6a1a3488774d97288dd1
sandyy-singh <sandeeprewa43@gmail.com>
2023-12-12 11:24:57 +0530

    some changes

--- src/components/dashboard/LineChart.jsx
+++ src/components/dashboard/LineChart.jsx
@@ -46,15 +46,20 @@ const LineChart = () => {
 
   return (
     <div className="container-fluid ">
-      <div className="row p-0 p-xxl-5">
-        <div className="col-12 mt-lg-5 p-0 p-xl-5">
+      <div className="row">
+        <div className="col-12 mt-lg-5 ">
           <div className="row">
-            <div className="col-12 ">
-              <h3 className="fs-4 fw-bold">Projected Growth</h3>
+            <div className="col-12 mt-5 mx-3 ">
+            
+            <div className="mt-xl-2">
+            <h3 className="fs-4 fw-bold">Projected Growth</h3>
             </div>
-
-            <div className="col-12 d-flex align-items-center justify-content-center ">
-              <Line data={data} />
+              
+            </div>
+            <div className="col-12 d-flex align-items-center justify-content-center p-md-4 mb-lg-4">
+              <div className="col-12 d-flex align-items-center justify-content-center p-md-4 mb-lg-4">
+                <Line data={data} />
+              </div>
             </div>
           </div>
         </div>
